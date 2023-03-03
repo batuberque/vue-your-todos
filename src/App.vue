6d9bb588a506174ddd68b2c031a16560cd446ff8
batuberque <batuberque@gmail.com>
2023-03-03 14:56:34 +0300

    feature: add styling for headers

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
+
 </script>
+
+<template>
+  <main class="main-wrapper">
+    <h1 class="page-title">Vue Todo</h1>
+    <p class="subtitle"> A todo app powered by vue3</p>
+    <input type="text">
+    <button>+ Add</button>
+  </main>
+</template>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    background-color: #fbfbfb;
+  }
+
+  .main-wrapper {
+    max-width: 600px;
+    margin: 0 auto;
+
+  }
+
+  .page-title {
+    font-family: 'DM Serif Display', serif;
+
+    font-size: 44px;
+    letter-spacing: 1.84px;
+    color: #2d2d2d;
+    margin-top: 104px;
+    margin-bottom: 0px;
+  }
+
+  .subtitle {
+    font-size: 1rem;
+    font-weight: bold;
+    color: #6b6b6b;
+    margin-top: 0;
+  }
+</style>
