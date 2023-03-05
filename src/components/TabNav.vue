3cd51755beef0e73832e791faa904ce5a610af3f
batuberque <batuberque@gmail.com>
2023-03-05 03:03:49 +0300

    feature: tabNav component configure

---
+++ src/components/TabNav.vue
@@ -0,0 +1,55 @@
+<script>
+import TabNavItem from './TabNavbarItem.vue'
+export default {
+    props: {
+        currentView: {
+            type: String,
+            default: 'All',
+            validator: value => {
+                return ['All', 'Current', 'Completed'].indexOf(value) !== -1
+            }
+        },
+        taskListOverview: {
+            type: Array,
+            required: true
+        }
+    },
+    components: {
+        TabNavItem
+    },
+    setup (props, ctx) {
+        const updateCurrentView = payload => {
+            ctx.emit('update-current-view', payload.value)
+        }
+
+        return {
+            updateCurrentView
+        }
+    }
+}
+</script>
+
+<template>
+    <nav>
+        <ul :class="$style['tab-nav-wrapper']">
+            <TabNavItem 
+                v-for="taskList in taskListOverview"
+                :key="`task-list-${taskList.name}`"
+                :listLength="taskList.length"
+                :listName="taskList.name"
+                :isActive="currentView === tasklist.name"
+                @emit-tab-value="updateCurrentView"
+            />
+        </ul>
+    </nav>
+</template>
+
+<style module>
+.tab-nav-wrapper {
+  display: flex;
+  column-gap: 30px;
+  list-style: none;
+  margin: 45px 0 20px;
+  padding: 0;
+}
+</style>
--- src/components/TabNavbarItem.vue
+++ src/components/TabNavbarItem.vue
@@ -2,74 +2,70 @@
 import { computed, useCssModule } from 'vue'
 
 export default {
-   props: {
-      isActive: {
-         type: Boolean,
-         default: false
-      },
-      listName: {
-         type: String,
-         required: true
-      },
-      listLength: {
-         type: Number,
-         required: true
-      }
-   },
-   setup (props, ctx) {
-      const style = useCssModule()
+  props: {
+    isActive: {
+      type: Boolean,
+      default: false
+    },
+    listName: {
+      type: String,
+      required: true
+    },
+    listLength: {
+      type: Number,
+      required: true
+    }
+  },
+  setup(props, ctx) {
+    const style = useCssModule()
 
-      const emitTabValue = () => {
-         ctx.emit('emit-tab-value', {
-            value: props.listName
-         })
-      }
-
-      const tabNavItemClassNames = computed(() => {
-         if (props.isActive) {
-            return [style.tabNavItem, style.isActive]
-         } else {
-            return style.tabNavItem
-         }
+    const emitTabValue = () => {
+      ctx.emit('emit-tab-value', {
+        value: props.listName
       })
+    }
 
-      return {
-         emitTabValue,
-         tabNavItemClassNames
+    const tabNavItemClassNames = computed(() => {
+      if (props.isActive) {
+        return [style.tabNavItem, style.isActive]
+      } else {
+        return style.tabNavItem
       }
-   }
+    })
 
-
-
-
-
+    return {
+      emitTabValue,
+      tabNavItemClassNames
+    }
+  }
 }
-
-
-
 </script>
 
 <template>
-   <li :class="tabNavItemClassNames">
-      <button :class="$style.tabNavItemButton" @click="emitTabValue">
-         {{  listName }} ({{ listLength }})
-      </button>
-   </li>
+  <li :class="tabNavItemClassNames">
+    <button :class="$style.tabNavItemButton" @click="emitTabValue">
+      {{ listName }} ({{ listLength }})
+    </button>
+  </li>
 </template>
 
-<script module>
+<style module>
 .tab-nav-item {
   padding-bottom: 2px;
 }
+
 .tab-nav-item:hover .tab-button {
   color: #0728bf;
 }
+
 .tab-nav-item.is-active {
   border-bottom: 3px solid #0631f8;
 }
+
 .tab-nav-item.is-active .tab-button {
   color: #2d2d2d;
 }
+
 .tab-nav-item-button {
   border: 0;
   background-color: transparent;
@@ -80,7 +76,8 @@ export default {
   padding: 0;
   font-size: 1rem;
 }
+
 .tab-nav-item-button:hover {
   cursor: pointer;
 }
-</script>
+</style>
